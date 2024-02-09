5d59b6fed20968dc912c8d91279dbf86abf8349e
Pavithradevi5599 <123530911+Pavithradevi5599@users.noreply.github.com>
2024-02-09 14:58:49 +0530

    Update Regex.py

--- Regex.py
+++ Regex.py
@@ -1,11 +1,11 @@
 import re
-html="<h1>Welcome to all</h1>"
-html2="<p>Hello</p>"
-html3="<h2>Good day</h2>"
-html4="<h3>Good morning</h3>"
-h=html+html2+html3+html4
+import requests
+from bs4 import BeautifulSoup
+url="https://www.w3schools.com/python"
+response=requests.get(url)
+html_content=response.text
 pattern="<.*?>(.*?)</.*?>"
-matches=re.findall(pattern,h)
+matches=re.findall(pattern,html_content)
 extracted_text='\n'.join(matches)
 with open('Regex.txt','w')as file:
     file.write(extracted_text)
